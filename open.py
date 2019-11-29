960f02e11c68ef3a9055ccb59931771025e1c83f
Vincent74230 <vincent.nowak@hotmail.fr>
2019-11-29 14:08:27 +0100

    update : request and data upload are in a class, in 'open' file :)

--- open.py
+++ open.py
@@ -2,6 +2,72 @@ import mysql.connector
 import requests
 
 
+
+class request:
+    def __init__(self):
+        self.res_json=[]
+
+    def import_products(self,categories):
+        l = len(categories)
+
+        for i in range (0,l):
+
+            payload = {'action':'process','json':1,'page_size':20,'tag_0':categories[i],
+            'tag_contains_0':'contains','tagtype_0':'categories','sort_by':'unique_scans_n'}
+
+            response = requests.get('https://fr.openfoodfacts.org/cgi/search.pl',
+            params = payload,headers = {'User-Agent':'Apptest - GNU/Linux - Version 0.1'})
+
+            response = response.json()
+            response = response['products']
+
+            self.res_json.append(response)
+
+def record_into_database(products,categories):
+    
+    connection = mysql.connector.connect(
+    host = 'localhost',
+    user = 'etudiant',
+    password = 'motdepasse',
+    database = 'pur_beurre'
+    )
+    kursor = connection.cursor()
+    add_product = ("INSERT INTO Product (id,name,nova,category,brand,stores) VALUES (%s,%s,%s,%s,%s,%s)")
+    
+
+    l_cat = len(products)
+    for i in range (0,l_cat):
+        n_products = len(products[i])
+        for x in range (0,n_products):
+            try:
+                data = (products[i][x]['id'],products[i][x]['product_name_fr'],products[i][x]['nova_groups'],categories[i],products[i][x]['brands'],products[i][x]['stores'])
+                kursor.execute(add_product,data)
+            except:
+                pass
+    
+    connection.commit()
+    kursor.close()
+    connection.close()
+
+categories = ['boissons avec sucre ajoute','pate a tartiner']#,'cafe','chips','chocolat']
+r= request()
+r.import_products(categories)
+
+record_into_database(r.res_json,categories)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 payload = {'action':'process','json':1,'page_size':50,'tag_0':'Boissons avec sucre ajouté',
 'tag_contains_0':'contains','tagtype_0':'categories','sort_by':'unique_scans_n'}
 
@@ -42,3 +108,4 @@ connection.commit()
 kursor.close()
 
 connection.close()
+"""
