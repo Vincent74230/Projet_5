ca310a6db76a9466b717760b8c43fbfd00e109f0
Vincent74230 <vincent.nowak@hotmail.fr>
2019-12-08 23:23:45 +0100

    REFACTORED the whole programm: much more clean. Last todo : requirements and sql_script

--- main.py
+++ main.py
@@ -15,66 +15,46 @@ research.kursor.execute("SELECT DISTINCT category FROM Product")
 categories = research.kursor.fetchall()
 for element in categories:
     research.categories.append(element[0])
+print (research.categories)
 
 print ("\n\t*-+/*-+/*-+/*-+Bienvenue dans ce programme de substitution alimentaire/*-+/*-+/*-+/*-+\n")
-print ("\t Ce programme propose une alternative plus saine à ce que vous mangez d'habitude")
+print ("\t Ce programme propose une alternative plus saine à ce que vous mangez d'habitude\n")
 
 while True:
-    print ("Veuillez choisir une catégorie :\n\n")
-    research.kursor.execute("SELECT no FROM Favourites")
-    research.favourites_id = research.kursor.fetchall()
+    print ("Veuillez faire un choix dans la liste ci-dessous:\n\n")
 
-    if research.favourites_id == []:
-        for i,category in enumerate(research.categories):
-            print ("Choix   {}:   n°{}".format(category,i+1))
-        cat_choice = research.secure_input(1,len(research.categories))
-    else:
-        print ("Voir vos resultats favoris : pressez 0\n")
-        for i in range (0,research.cat_len):
-            print ("Choix   {}:   n°{}".format(research.categories[i],i+1))
-        cat_choice = research.secure_input(0,research.cat_len)
-    
-    if cat_choice == 0:
+    for i,element in enumerate (research.categories):
+        print ("N°{} : {}".format(i,element))
+    categoy_choice = research.secure_input(0,len(research.categories)-1)
+    print (categoy_choice)
+
+    if categoy_choice == 0:
         research.fetch_favourites()
-
-    research.fetch_examples_in_db(cat_choice)
-
-    print ("Veuillez choisir le produit que vous souhaitez substituer:")
-    for i in range (0,len(research.temporary_list)):
-        print ("Choix n° {} : Nom : {}, marque : {}, indice nova : {}".format(i+1,research.temporary_list[i][1],research.temporary_list[i][2],research.temporary_list[i][3]))
+    else:
+        research.fetch_examples_in_db(categoy_choice)
     
-    product_choice = research.secure_input(1,len(research.temporary_list))
-
-    research.fetch_substitutes(product_choice)
+    print ("Voulez-vous enregistrer le resultat de votre recherche dans vos favorits?\nOui tapez 1, non tapez 0")
     
-    print ("\nVoici le resultat de votre recherche pour : ")
-    print ("{}, marque:  {}, indice nova : {}\n".format(research.temporary_list[product_choice-1][1],research.temporary_list[product_choice-1][2],research.temporary_list[product_choice-1][3]))
-    print ("Substituts trouvés:")
-    for i in range (0,len(research.substitutes)):
-        research.kursor.execute("SELECT name,brand,nova,stores,id FROM Product WHERE id = %s",(research.substitutes[i],))
-        total = research.kursor.fetchall()
-        print ("\nNom du produit : {}, marque:  {}, indice nova : {}, ce produit est en vente dans les enseignes : {}.\n Lien vers une description complete : https://fr.openfoodfacts.org/produit/{}\n".format(total[0][0],total[0][1],total[0][2],total[0][3],total[0][4]))
-    
-    print ("Voulez-vous enregistrer le resultat de votre recherche dans vos favorits?\nOui tapez 1, non tapez 2")
 
-    logging = research.secure_input(1,2)
+    logging = research.secure_input(0,1)
 
-    if logging == 2:
+
+    if logging == 0:
         pass
     elif logging == 1:
         research.log_in_bd()
         connection.commit()
 
-    print ("Voulez-vous effectuer une nouvelle recherche ?\nOui tapez 1, non tapez 2")
-    again = research.secure_input(1,2)
-    if again == 2:
+    print ("Voulez-vous effectuer une nouvelle recherche ?\nOui tapez 1, non tapez 0")
+    again = research.secure_input(0,1)
+    if again == 0:
         break
     elif again == 1:
         research.substitutes = []
         research.temporary_list = []
         continue
     break
-    
 
+    
 research.kursor.close()
 connection.close()
--- read_n_write.py
+++ read_n_write.py
@@ -2,10 +2,8 @@ import random
 
 class Database:
     def __init__(self):
-        self.categories = []
-        self.cat_len = 0
-        self.substitutes = []
-        self.temporary_list = []
+        self.categories = ['Voir vos favorits']
+        self.sub_list = []
         self.user_first_choice_id = 0
         self.favourites_id = []
         self.kursor = 0
@@ -26,30 +24,48 @@ class Database:
         return choice
 
     def fetch_examples_in_db(self,cat_choice):#fetches 10 products in bd
-        self.kursor.execute("SELECT id,name,brand,nova,category FROM Product WHERE nova = '4' AND category = %s",(self.categories[cat_choice-1],))
+        ten_examples = []
+        self.kursor.execute("SELECT id FROM Product WHERE nova = 4 AND category = %s",(self.categories[cat_choice],))
         examples = self.kursor.fetchall()
         for i in range (0,10):
-            rand = random.randint(0,len(examples))
-            self.temporary_list.append(examples[rand]) #todo later : check to pick unique numbers in that list
+            ten_examples.append(examples[random.randint(0,len(examples))][0])
+        for i,element in enumerate (ten_examples):
+            print ("Choix n°{}".format(i+1))
+            self.display_product_from_id(element)
 
-    def fetch_substitutes(self,product_choice):
-        self.user_first_choice_id = self.temporary_list[product_choice-1][0]
-        user_first_choice_category = self.temporary_list[product_choice-1][4]
+        print ("Veuillez choisir le produit que vous voulez substituer:")
+        product_choice = self.secure_input(1,len(ten_examples))
+        user_choice_id = ten_examples[product_choice-1]
+        self.user_first_choice_id = user_choice_id
+        self.fetch_substitutes(user_choice_id,cat_choice)
+        
 
+    def display_product_from_id(self,product_id):
+        self.kursor.execute("SELECT name,brand,nova,stores,id FROM Product WHERE id = %s",(product_id,))
+        response = self.kursor.fetchall()
+        response = response[0]
+        print ("{} de la marque {} (indice nova : {}), disponible dans les magasins {}.\nLien vers une description complete https://fr.openfoodfacts.org/produit/{}\n".format(response[0],response[1],response[2],response[3],response[4]))
+
+    def fetch_substitutes(self,product_choice_id,cat_choice):
+        category = self.categories[cat_choice-1]
         for i in range (1,4):
-            try:
-                self.kursor.execute ("SELECT id FROM Product WHERE nova = %s AND category = %s",(i,user_first_choice_category))
-                substitute = self.kursor.fetchall()
-                rand = random.randint (0,len(substitute))
-                self.substitutes.append(substitute[rand][0])
-            except:
-                pass
+            self.kursor.execute("SELECT id FROM Product WHERE nova = %s AND category = %s",(i,category))
+            response = self.kursor.fetchall()
+            if response != []:
+                self.sub_list.append(response[random.randint(0,len(response))][0])
 
+        print ("Voici les substituts trouvés pour:")
+        self.display_product_from_id(product_choice_id)
+        for element in self.sub_list:
+            self.display_product_from_id(element)
+            
     def log_in_bd(self):
-        string_substitutes = ",".join(self.substitutes)
-        add_ids = ("INSERT INTO Favourites (no,sub) VALUES (%s,%s)")
-        data_ids = (self.user_first_choice_id,string_substitutes)
-        self.kursor.execute(add_ids,data_ids)
+        string_substitutes = ",".join(self.sub_list)
+        add = ("INSERT INTO Favourites (no,sub) VALUES (%s,%s)")
+        data = (self.user_first_choice_id,string_substitutes)
+        self.kursor.execute(add,data)
+
+
 
     def fetch_favourites(self):
         while True:
